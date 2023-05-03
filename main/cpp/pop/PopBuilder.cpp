5df58f1abc32c9c854455f493ef05c667f2b143e
asierens <122800553+asierens@users.noreply.github.com>
2023-05-03 14:32:50 +0200

    print type

--- main/cpp/pop/PopBuilder.cpp
+++ main/cpp/pop/PopBuilder.cpp
@@ -227,7 +227,8 @@ shared_ptr<Population> PopBuilder::Build(shared_ptr<Population> pop)
         // Initialize poolSys with empty ContactPools (even for Id=0).
         // --------------------------------------------------------------
         for (Id typ : IdList) {
-                m_stride_logger->info(typ);
+                stingtyp = ToString(typ);
+                m_stride_logger->info("typ {}.", stringtyp);
                 for (unsigned int i = 1; i < maxIds[typ] + 1; i++) {
                         pop->RefPoolSys().CreateContactPool(typ);
                 }
