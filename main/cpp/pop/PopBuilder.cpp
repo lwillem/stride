50ce29a94b28956f01f073c3f967f6ccd93d1d2f
asierens <122800553+asierens@users.noreply.github.com>
2023-05-03 23:15:19 +0200

    correction of access value in map

--- main/cpp/pop/PopBuilder.cpp
+++ main/cpp/pop/PopBuilder.cpp
@@ -330,7 +330,7 @@ shared_ptr<Population> PopBuilder::Build(shared_ptr<Population> pop)
                         const auto person_id            = static_cast<unsigned int>(IntFromString(values[0]));
                         const auto INS_j_id             = static_cast<unsigned int>(IntFromString(values[1]));
 
-                        auto p=id_pointer_persons.find(person_id);
+                        Person* p=id_pointer_persons[person_id];
                         pop->RefPoolSys().RefPools(typ)[INS_j_id].AddMember(&p);
                         
 
