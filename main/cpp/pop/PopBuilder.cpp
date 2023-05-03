7114b2b4c3c8f715b51c4abe8b6ae08867039e1b
asierens <122800553+asierens@users.noreply.github.com>
2023-05-03 14:11:53 +0200

    adding print

--- main/cpp/pop/PopBuilder.cpp
+++ main/cpp/pop/PopBuilder.cpp
@@ -218,42 +218,8 @@ shared_ptr<Population> PopBuilder::Build(shared_ptr<Population> pop)
                         if (typ != Id::NewCommunity) {
                         maxIds[typ] = max(maxIds[typ], p.GetPoolId(typ));
                         }
-                        else {                                
-                        const auto fileName = m_config.get<string>("run.new_community_file");
-                        m_stride_logger->info("Reading max number of New Communities from file {}.", fileName);
-
-                        const auto use_install_dirs = m_config.get<bool>("run.use_install_dirs");
-                        const auto filePath         = (use_install_dirs) ? FileSys::GetDataDir() /= fileName : filesys::path(fileName);
-                        if (!is_regular_file(filePath)) {
-                                throw runtime_error(string(__func__) + "> New community file " + filePath.string() + " not present.");
-                        }
-
-                        ifstream newCommunityFile;
-                        newCommunityFile.open(filePath.string());
-                        if (!newCommunityFile.is_open()) {
-                        throw runtime_error(string(__func__) + "> Error opening new community file " + filePath.string());
-                        }
-
-                        string line;
-                        getline(newCommunityFile, line); // step over file header
-                        auto headers   = Split(line, ",");
-
-
-                        getline(newCommunityFile, line);
-                        const auto values               = Split(line, ",");
-                        const auto person_id            = static_cast<unsigned int>(IntFromString(values[0]));
-                        const auto INS_j_id             = static_cast<unsigned int>(IntFromString(values[1]));
-                          
-                        maxIds[typ] = INS_j_id;
-
-                        m_stride_logger->trace("Done determining max number of New Communities.");
-
-                        newCommunityFile.close();
-
-                        m_stride_logger->trace("Done determining max number of New Communities.");
-
-                        }
-
+                        
+        
 
                 }
         }
@@ -261,6 +227,7 @@ shared_ptr<Population> PopBuilder::Build(shared_ptr<Population> pop)
         // Initialize poolSys with empty ContactPools (even for Id=0).
         // --------------------------------------------------------------
         for (Id typ : IdList) {
+                m_stride_logger->info(typ)
                 for (unsigned int i = 1; i < maxIds[typ] + 1; i++) {
                         pop->RefPoolSys().CreateContactPool(typ);
                 }
