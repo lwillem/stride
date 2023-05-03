4468266daa12fa0acc76fbb46ab88d0ed56e03b0
asierens <122800553+asierens@users.noreply.github.com>
2023-05-03 23:53:32 +0200

    putting new communities on true + removing printing of type of id

--- main/cpp/pop/PopBuilder.cpp
+++ main/cpp/pop/PopBuilder.cpp
@@ -274,7 +274,6 @@ shared_ptr<Population> PopBuilder::Build(shared_ptr<Population> pop)
         // Initialize poolSys with empty ContactPools (even for Id=0).
         // --------------------------------------------------------------
         for (Id typ : IdList) {
-                m_stride_logger->info("typ {}.", ToString(typ));
                 for (unsigned int i = 1; i < maxIds[typ] + 1; i++) {
                         pop->RefPoolSys().CreateContactPool(typ);
                 }
