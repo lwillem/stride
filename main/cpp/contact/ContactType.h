c9a4bf2905f1276967cb9fd187d7ac22fb3ad0f8
asierens <122800553+asierens@users.noreply.github.com>
2023-05-03 15:04:07 +0200

    adding new_community to idpack

--- main/cpp/contact/ContactType.h
+++ main/cpp/contact/ContactType.h
@@ -70,7 +70,7 @@ struct IDPack
 
 /// Placed separately to please swig and avoid syntax errors there
 using IdPack_t =
-    IDPack<Id::Household, Id::K12School, Id::College, Id::Workplace, Id::PrimaryCommunity, Id::SecondaryCommunity, Id::HouseholdCluster, Id::Collectivity>;
+    IDPack<Id::Household, Id::K12School, Id::College, Id::Workplace, Id::PrimaryCommunity, Id::SecondaryCommunity, Id::HouseholdCluster, Id::Collectivity, Id::NewCommunity>;
 
 /// A constexpr global variable that gives access to the available Ids
 constexpr IdPack_t IdPack;
