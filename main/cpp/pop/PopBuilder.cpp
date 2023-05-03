dd42352d8b865074a1589992037a57791e5a575b
asierens <122800553+asierens@users.noreply.github.com>
2023-05-03 14:15:31 +0200

    adding ;

--- main/cpp/pop/PopBuilder.cpp
+++ main/cpp/pop/PopBuilder.cpp
@@ -227,7 +227,7 @@ shared_ptr<Population> PopBuilder::Build(shared_ptr<Population> pop)
         // Initialize poolSys with empty ContactPools (even for Id=0).
         // --------------------------------------------------------------
         for (Id typ : IdList) {
-                m_stride_logger->info(typ)
+                m_stride_logger->info(typ);
                 for (unsigned int i = 1; i < maxIds[typ] + 1; i++) {
                         pop->RefPoolSys().CreateContactPool(typ);
                 }
