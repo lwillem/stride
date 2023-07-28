cfa916184044cfc94d522d3e0061093633246cf3
asierens <122800553+asierens@users.noreply.github.com>
2023-07-28 11:46:08 +0200

    adapt function StartInfection that it registers the pool where the infection took place

--- main/cpp/disease/Health.h
+++ main/cpp/disease/Health.h
@@ -114,7 +114,7 @@ public:
         void SetSusceptible() { m_status = HealthStatus::Susceptible; }
 
         /// Start the infection.
-        void StartInfection(unsigned int id_index_case, unsigned int id_infector, 
+        void StartInfection(unsigned int id_index_case, unsigned int id_infector, ContactType::Id infection_poolType,
                 double relative_infectiousness);
 
         /// Stop the infection.
