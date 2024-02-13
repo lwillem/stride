8ce96c0ee5dc9cd47bb8cc8df62a176bc891e35f
asierens <122800553+asierens@users.noreply.github.com>
2024-02-13 17:34:06 +0100

    contacts at workplace based on workplace size instead of contact data, in order to have a more correct average number of contacts at work

--- main/cpp/contact/Infector.cpp
+++ main/cpp/contact/Infector.cpp
@@ -249,14 +249,25 @@ inline double GetContactProbability(const AgeContactProfile& profile, const Pers
         double reference_num_contacts_p1;
         double reference_num_contacts_p2;
         
-	// get the reference number of contacts, given age and age-contact profile
-        if (pType != Id::OtherHouse && pType != Id::RestoCafe && pType != Id::OtherPlace && pType != Id::Transport) {
+	// get the reference number of contacts, given age and age-contact profile     
+       if (pType == Id::Workplace) {
+        if (pool_size < 21){
+                reference_num_contacts_p1 = pool_size-1;
+                reference_num_contacts_p2 = pool_size-1;
+        } else {
+                reference_num_contacts_p1 = 19;
+                reference_num_contacts_p2 = 19;
+        }
+       }
+       else if (pType != Id::OtherHouse && pType != Id::RestoCafe && pType != Id::OtherPlace && pType != Id::Transport) {
            reference_num_contacts_p1 = profile[EffectiveAge(static_cast<unsigned int>(p1->GetAge()))];
            reference_num_contacts_p2 = profile[EffectiveAge(static_cast<unsigned int>(p2->GetAge()))];
-        } else {
+        } 
+        else {
            reference_num_contacts_p1 = p1->CPoolContacts(pType)[dayWeek];
            reference_num_contacts_p2 = p2->CPoolContacts(pType)[dayWeek];
-        }
+                }
+        
         const double potential_num_contacts{static_cast<double>(pool_size - 1)};
 
         // adjust contact for distancing
