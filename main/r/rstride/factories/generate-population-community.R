909a133ad86f0d7a28d8f432f54200c950b5d11a
Regina Manansala <reginamanansala@Reginas-UA-Macbook.local>
2026-07-07 14:38:18 +0200

    update script to speed up function

--- main/r/rstride/factories/generate-population-community.R
+++ main/r/rstride/factories/generate-population-community.R
@@ -27,7 +27,7 @@ get_distance <- function(f_x_coord1,f_y_coord1,f_x_coord2,f_y_coord2){
   
 }
 
-generate_population_community <- function(pop_data,pop_settings,district_data)
+generate_population_community <- function(pop_data,pop_settings,district_data,district_distance)
 {
   
   # INITIALISE
@@ -49,37 +49,40 @@ generate_population_community <- function(pop_data,pop_settings,district_data)
     pop_data$com_distance <- NA
     pop_data$com2_distance <- NA
     # ASSIGN PEOPLE TO COMMUNITIES
-    # per district, select one of the 3 nearest communities
-    i_district <- 2
-    for(i_district in 1:nrow(district_distance)){
-      # get people from one district and temporary store district features
+    
+    # PRE-GROUP: build district -> row index list ONCE (avoids repeated full-vector scans)
+    district_idx <- split(seq_len(nrow(pop_data)), pop_data$stcotrbg)
+    
+    # PRE-INDEX: lookup district coords by GEOID10 ONCE
+    district_lookup <- setNames(seq_len(nrow(district_data)), district_data$GEOID10)
+    
+    for(i_district in seq_len(nrow(district_distance))){
       bg10 <- rownames(district_distance)[i_district]
-      flag_district    <- pop_data$stcotrbg == bg10
-      district_x_coord <- district_data[district_data$GEOID10 == bg10, "latitude"] #unique(pop_data$latitude[flag_district])
-      district_y_coord <- district_data[district_data$GEOID10 == bg10, "longitude"] #unique(pop_data$longitude[flag_district])
-      district_size    <- sum(flag_district)
       
-      if(district_size > 0){
-        # get distances from district 'i' to all communities
-        distances <- get_distance(district_x_coord$latitude,district_y_coord$longitude,community_data$latitude,community_data$longitude)
-        # select com_id from nearest 3 communities
-        sel_com_id <- which(distances %in% (sort(unique(distances))[1:3]))
-        # calculate probability based on distance ranking: 1/rank
-        sel_com_prob <- rep(1,length(sel_com_id))
-        # sel_com_prob <- 1/order(distances[sel_com_id])
-        # standardize probabilities
-        sel_com_prob <- sel_com_prob / sum(sel_com_prob)
-        # sample from community selection, with replacement
-        pop_data$com_id[flag_district]  <- sample(sel_com_id,district_size,replace=T,prob=sel_com_prob)
-        pop_data$com2_id[flag_district] <- sample(sel_com_id,district_size,replace=T,prob=sel_com_prob)
-        
-        pop_data$com_distance[flag_district] <- distances[pop_data$com_id[flag_district]]
-        pop_data$com2_distance[flag_district] <- distances[pop_data$com2_id[flag_district]]
-        
-      }
+      idx <- district_idx[[bg10]]
+      if(is.null(idx)) next
+      district_size <- length(idx)
+      if(district_size == 0) next
+      
+      dd_row <- district_lookup[bg10]
+      district_x_coord <- district_data$latitude[dd_row]
+      district_y_coord <- district_data$longitude[dd_row]
+      
+      distances <- get_distance(district_x_coord, district_y_coord,
+                                community_data$latitude, community_data$longitude)
+      
+      sel_com_id   <- order(distances)[1:3]
+      sel_com_prob <- rep(1, length(sel_com_id))
+      sel_com_prob <- sel_com_prob / sum(sel_com_prob)
+      
+      pop_data$com_id[idx]  <- sample(sel_com_id, district_size, replace = TRUE, prob = sel_com_prob)
+      pop_data$com2_id[idx] <- sample(sel_com_id, district_size, replace = TRUE, prob = sel_com_prob)
+      
+      pop_data$com_distance[idx]  <- distances[pop_data$com_id[idx]]
+      pop_data$com2_distance[idx] <- distances[pop_data$com2_id[idx]]
     }
     
-    names(pop_data)
+    # names(pop_data)
     ## UPDATE COMMUNITY DATA
     community_data$size1 <- 0
     community_data$size2 <- 0 
@@ -108,26 +111,19 @@ generate_population_community <- function(pop_data,pop_settings,district_data)
     
     
     ## EXPLORE DISTANCE FROM HOME
-    tbl_dist <- table(cut(c(pop_data$com_distance,pop_data$com2_distance),c(0,.002,.004,.006,.02),right=F))
-    barplot(tbl_dist/sum(tbl_dist),
-            xlab='Distance from home',
-            ylab='Fraction of the population')
+    tbl_dist <- table(cut(c(pop_data$com_distance, pop_data$com2_distance), c(0,.002,.004,.006,.02), right = FALSE))
+    barplot(tbl_dist / sum(tbl_dist), 
+            xlab = 'Distance from home', 
+            ylab = 'Fraction of the population')
     
-    ## EXPLORE COMMUNITY OVERLAP
-    opt <- matrix(0,nrow=num_com,ncol=num_com)
-    i <- 1
-    for(i in 1:num_com){
-      tmp <- table(pop_data$com2_id[pop_data$com_id == i])
-      opt[i,as.numeric(names(tmp))] <- tmp
-    }
+    # FAST community overlap: single crosstab instead of per-community loop
+    overlap_tbl <- table(factor(pop_data$com_id, levels = 1:num_com),
+                         factor(pop_data$com2_id, levels = 1:num_com))
+    opt <- matrix(overlap_tbl, nrow = num_com, ncol = num_com)
     
-    plot(sort(opt),
-         main=paste0('community overlap (',num_com,'x',num_com,')'),
-         xlab="Community (sorted)",
-         ylab='Overlap: count')
-    
-    hist(opt[opt>0],500,xlab='Community overlap (if >0)',
-         main='Community overlap (if >0)')
+    plot(sort(opt), main = paste0('community overlap (', num_com, 'x', num_com, ')'),
+         xlab = "Community (sorted)", ylab = 'Overlap: count')
+    hist(opt[opt > 0], 500, xlab = 'Community overlap (if >0)', main = 'Community overlap (if >0)')
     
   } # end if community size > 0
   
@@ -135,31 +131,21 @@ generate_population_community <- function(pop_data,pop_settings,district_data)
   df_sf <- st_as_sf(district_data, coords = c("longitude", "latitude"), crs = 4326)
   community_data_sf <- st_as_sf(community_data, coords = c("y_coord", "x_coord"), crs = 4326)
   gmap <- ggplot() +
-    # geom_sf(data = world, fill = "gray95") +
     geom_sf(data = df_sf, color = "red", size = 2) +
     geom_sf(data = community_data_sf, color = "blue", size = 1) +
     theme_minimal()
   print(gmap)
   
-  barplot(table(cut(community_data$size1,seq(0,2,0.2)*com_target_size))/num_com,
-          xlab='community size',
-          ylab='fraction',
-          las=2,
-          cex.names = 0.6)
+  barplot(table(cut(community_data$size1, seq(0,2,0.2) * com_target_size)) / num_com,
+          xlab = 'community size', ylab = 'fraction', las = 2, cex.names = 0.6)
   
-  barplot(rbind(sort(table(pop_data$com_id)),
-                sort(table(pop_data$com2_id))),
-          beside=T,
-          xlab='community (sorted)',
-          ylab='size',
-          xaxt='n')
-  abline(h=com_target_size)
-  legend('topleft',c('community 1','community 2'),fill=c(1,8))
+  barplot(rbind(sort(table(pop_data$com_id)), sort(table(pop_data$com2_id))),
+          beside = TRUE, xlab = 'community (sorted)', ylab = 'size', xaxt = 'n')
+  abline(h = com_target_size)
+  legend('topleft', c('community 1','community 2'), fill = c(1,8))
   
-  hist(c((table(pop_data$com_id)),
-         (table(pop_data$com2_id))),
-       xlab='community size')
-  
+  hist(c(table(pop_data$com_id), table(pop_data$com2_id)), xlab = 'community size')
+
   # return pop_data and community_data
   return(list(pop_data,community_data))
   
