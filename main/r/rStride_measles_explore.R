d6e1632caf24c37f808891a1257f7518afd61dbb
Regina Manansala <reginamanansala@Reginas-UA-Macbook.local>
2026-07-30 09:28:03 +0200

    reconcile rstride_measles_explore.R with LW updates to include immunity

--- main/r/rStride_measles_explore.R
+++ main/r/rStride_measles_explore.R
@@ -14,7 +14,7 @@
 #  see http://www.gnu.org/licenses/.
 #
 #
-#  Copyright 2018, Willem L, Kuylen E & Broeckhove J
+#  Copyright 2026, Manansala R, Willem L
 #############################################################################
 #
 # Call this script from the main project folder (containing bin, config, lib, ...)
@@ -31,72 +31,82 @@ rm(list=ls())
 source('./bin/rstride/rStride.R')
 
 # set directory postfix (optional)
-dir_postfix <- '_expl'
+dir_postfix <- '_expl_measles'
 
 ##################################
-## DESIGN OF EXPERIMENTS        ##
+## DEFAULT PARAMETERS        ##
 ##################################
 
-# uncomment the following line to inspect the config xml tags
-#names(xmlToList('./config/run_default.xml'))
+# get default parameters and values
+exp_param_list <- get_default_param()
 
-# add default parameters and values
-# exp_param_list <- get_measles_default_param()
+################################################ #
+## UPDATE PARAMETERS VALUES         ####
+################################################ #
 
-# set the number of realisations per configuration set
-num_seeds<- 5
+# number of stochastic realisations
+exp_param_list$num_rng_seeds <- 2
+
+# simulation horizon and start
+exp_param_list$num_days   <- 40
+exp_param_list$start_date <- "2023-01-01"
+
+# case study for measles
+exp_param_list$r0 <- 12
+exp_param_list$disease_config_file <- "data/disease_measles_usa.xml"
+
+# hospital settings
+exp_param_list$hosp_probability_factor <- 0.1 #TODO: set real value
+exp_param_list$hospital_length_of_stay <- 14  #TODO: set real value  
+exp_param_list$hospital_probability_age       #TODO: set age-specific adjustment of 'hosp_probability_factor'
+exp_param_list$hospital_category_age          #FYI: age categories for 'hosp_probability_factor' adjustments
+
+# USA population
+exp_param_list$age_contact_matrix_file <- "data/contact_matrix_usa_conditional.xml"
+exp_param_list$holidays_file           <- "data/calendar_dane_2023_2026_measles.csv"
+exp_param_list$population_file         <- "data/pop_usa_wisconsin_dane474k_c1000.csv"
+
+# initial conditions
+exp_param_list$num_infected_seeds <- 20
+exp_param_list$seeding_age_min    <- 1   # infants <1y cannot be selected as index case
+exp_param_list$seeding_age_max    <- 99
+
+# log level(s). Options: Incidence, Transmissions, Contacts, None
+exp_param_list$event_log_level <- "Transmissions"
+
+# reference data file(s)
+exp_param_list$reference_hospital_data_file <- NA
+exp_param_list$reference_serology_data_file <- NA
+
+# # immunity profile = starting condition (time consuming!)
+exp_param_list$immunity_profile <- "AgeDependent"
+exp_param_list$immunity_distribution_file <- "data/immunity_measles_dummy.xml" # "data/immunity_measles_belgium.xml" #c("data/immunity_measles_belgium.xml","data/immunity_measles_belgium_dummy.xml")
+exp_param_list$immunity_link_probability <- 0 # immunity is distributed by household, this is the chance of continuing to immunize the next shuffled household member instead of jumping to a new random household
+
+# virtual survey for immunity levels
+exp_param_list$num_participants_survey
+exp_param_list$contact_survey_dates <- exp_param_list$start_date # if log level is not "Contacts", only health data is obtained
 
 ################################################ #
 ## GENERATE DESIGN OF EXPERIMENT GRID         ####
 ################################################ #
 
-# # add parameters and values to combine in a full-factorial grid
-# exp_design <- expand.grid(r0                            = seq(12,14,2),
-#                           num_days                      = c(40,50),
-#                           rng_seed                      = seq(num_seeds),
-#                           track_index_case              = 'false',
-#                           contact_log_level             = "Transmissions",
-#                           seeding_rate                  = 0.00002,
-#                           disease_config_file           = "data/disease_measles_adaptive_behavior.xml",
-#                           population_file               = "data/pop_flanders600.csv",
-#                           age_contact_matrix_file       = "data/contact_matrix_flanders_subpop.xml",
-#                           adaptive_symptomatic_behavior = 'true',
-#                           stringsAsFactors = F)
-
 # get grid-based design of experiments
-# add parameters and values to combine in a full-factorial grid
-exp_design <- expand.grid(r0                            = seq(12,14,2),
-                          num_days                      = c(40,50),
-                          rng_seed                      = seq(num_seeds),
-                          age_contact_matrix_file       = "sim_output/20260710_111118_WI-Dane_conditional_social_contacts/contact_matrix_usa_conditional.xml",
-                          disease_config_file           = "data/disease_measles.xml",
-                          holidays_file                 = "data/calendar_dane_2023_2026_measles.csv",
-                          immunity_profiles             = "AgeDependent",
-                          immunity_distribution_file    = "data/immunity_measles_WI.xml", 
-                          # immunity_rate                 = 0.8,
-                          # num_participants_survey       = 5000,
-                          population_file               = "sim_output/20260710_111118_WI-Dane_conditional_social_contacts/20260710_111118_population_WI-Dane.csv",
-                          seeding_age_max               = 99,
-                          seeding_age_min               = 1,
-                          seeding_rate                  = 0.00002,
-                          num_infected_seeds            = 9,
-                          start_date                    = "2023-02-01",
-                          track_index_case              = 'false',
-                          event_log_level               = "Transmissions",
-                          stride_log_level              = "true",
-                          adaptive_symptomatic_behavior = 'true',
-                          stringsAsFactors = F)
-
+exp_design <- .rstride$get_full_grid_exp_design(exp_param_list = exp_param_list,
+                                                num_rng_seeds  = exp_param_list$num_rng_seeds)
 dim(exp_design)
 
-# add a unique seed for each run
-set.seed(125)
-exp_design$rng_seed <- sample(1e4,nrow(exp_design))
+# tmp fix:
+if(any(is.na(exp_param_list$immunity_distribution_file))){
+  exp_param_list$immunity_distribution_file <- NULL
+}
 
 ##################################
 ## RUN rSTRIDE                  ##
 ##################################
-project_dir <- run_rStride(exp_design,dir_postfix)
+project_dir <- run_rStride(exp_design,
+                           dir_postfix,
+                           remove_run_output = FALSE)
 
 
 #####################################
@@ -116,4 +126,19 @@ inspect_participant_data(project_dir)
 ##################################
 inspect_transmission_dynamics(project_dir)
 
-inspect_transmission_data(project_dir)
+
+############################# #
+## INCIDENCE DATA          ####
+############################# #
+inspect_incidence_data(project_dir)
+
+
+############################# #
+## PREVALENCE              ####
+############################# #
+inspect_prevalence_data(project_dir)
+
+
+# TMP: explore burden of disease
+project_summary    <- .rstride$load_project_summary(project_dir)
+data_incidence_all <- .rstride$load_aggregated_output(project_dir,'data_incidence')
