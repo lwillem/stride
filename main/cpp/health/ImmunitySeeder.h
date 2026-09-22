5328a090b1e847984ff898865b26c5a2cad5740c
Regina Manansala <reginamanansala@Reginas-UA-Macbook.local>
2026-09-22 11:09:59 +0200

    Revert to original immunity method

--- main/cpp/health/ImmunitySeeder.h
+++ main/cpp/health/ImmunitySeeder.h
@@ -1,5 +1,5 @@
 /*
- *  This is free software: you can redistribute it and/or modify it
+*  This is free software: you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  any later version.
@@ -27,39 +27,34 @@
 
 #include "util/Ptree.h"
 #include <memory>
-#include <unordered_set>
 
 namespace stride {
 
-/**
- * Seeds population w.r.t immunity (natural immunity, vaccination, ...)
- */
-class ImmunitySeeder
-{
-public:
+	/**
+	 * Seeds population w.r.t immunity (natural immunity, vaccination, ...)
+	 */
+	class ImmunitySeeder
+	{
+	public:
 		/// Initializing ImmunitySeeder.
-        explicit ImmunitySeeder(const stride::util::ptree& config, std::shared_ptr<util::RnMan> rnMan);
+		explicit ImmunitySeeder(const stride::util::ptree& config, std::shared_ptr<util::RnMan> rnMan);
 
-        /// Build the simulator.
-        void Seed(std::shared_ptr<Population> pop);
+		/// Build the simulator.
+		void Seed(std::shared_ptr<Population> pop);
 
-private:
-        /// Seed for vaccination/natural immunity.
-        void Vaccinate(const std::string& immunityType, const std::string& immunizationProfile,
-                       const util::SegmentedVector<ContactPool>& immunityPools, std::shared_ptr<Population> pop);
+	private:
+		/// Seed for vaccination/natural immunity.
+		void Vaccinate(const std::string& immunityType, const std::string& immunizationProfile,
+					   const util::SegmentedVector<ContactPool>& immunityPools, std::shared_ptr<Population> pop);
 
-        /// Random immunization.
+		/// Random immunization.
 		void Random(const util::SegmentedVector<ContactPool>& pools, std::vector<double>& immunityDistribution,
 					double immunityLinkProbability, std::shared_ptr<Population> pop, const bool log_immunity);
 
-        /// Flag a subset of households as vaccine-hesitant; their children are excluded
-        /// from the vaccine pass regardless of the age-specific rate.
-        void MarkHesitantHouseholds(std::shared_ptr<Population> pop);
 
-private:
+	private:
 		const stride::util::ptree& m_config; ///< Run config.
 		std::shared_ptr<util::RnMan> m_rn_man; ///< Random number manager.
-        std::unordered_set<unsigned int> m_hesitant_households; ///< IDs of households refusing vaccination for their children.
-};
+	};
 
 } // namespace stride
